afe29893224fb5084cc91028c43f04f2a4353c48
Josh Long <josh@joshlong.com>
2018-10-11 23:01:58 -0700

    up

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,6 @@ fi
 ce_dir=$(cd $(dirname $0) && pwd)
 ce_bin_dir=${ce_dir}/bin/${platform}
 export PATH=$PATH:$ce_bin_dir
-echo $ce_bin_dir
-
-ls -la $ce_bin_dir
 
 cf login -a $CF_API -u $CF_USER -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE
 cf push -p . rsb-site
