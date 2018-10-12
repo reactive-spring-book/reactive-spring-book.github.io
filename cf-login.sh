7fa54bc1c72e16f20945d7d291bf7acf8b7fedf2
Josh Long <josh@joshlong.com>
2018-10-11 22:09:34 -0700

    deployment.

--- cf-login.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export CF_SPACE=cloud-native-java
-./cf login -a $CF_API -u $CF_USER -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE
---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+platform='unknown'
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+   platform='freebsd'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+    platform='osx'
+fi
+
+ce_dir=$(cd $(dirname $0) && pwd)
+ce_bin_dir=${ce_dir}/bin
+export PATH=$PATH:${ce_bin_dir}
+echo $ce_bin_dir
+
+cf login -a $CF_API -u $CF_USER -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE
+cf push -p . rsb-site
+
